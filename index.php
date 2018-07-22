76927cdc78c2dbe354e098b6771d9fd8ba45fc31
Steven Hayes <32312887+stevenhayes97@users.noreply.github.com>
2018-07-22 11:52:32 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+
+		<!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame
+		Remove this if you use the .htaccess -->
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+		<title>River Falls Hosptial | Home</title>
+		<meta name="description" content="">
+		<meta name="author" content="lifea">
+		
+		<meta name="viewport" content="width=device-width; initial-scale=1.0">
+
+		<!-- Replace favicon.ico & apple-touch-icon.png in the root of your domain and delete these references -->
+		<link rel="shortcut icon" href="/favicon.ico">
+		<link rel="apple-touch-icon" href="/apple-touch-icon.png">
+		<link rel="stylesheet" href="styles.css"> 
+	</head>
+
+	<body background="https://images.freecreatives.com/wp-content/uploads/2015/04/3336-grey-texture-1920x1080-abstract-wallpaper.gif">
+		<div>
+			<header>
+				<h1 >River Falls Hospital Home </h1>
+				<div id="links">
+				<ul id = "links">
+					<li><a href="index.php">Home </a></li>
+					<li><a href="login.php">Log In</a></li>
+					<li><a href="PatientInformation.php"> Patient Entry </a></li>
+					<li><a href="newemployeeform.php">Employee Entry</a></li>
+					<li><a href="patients.php">View Patients</a></li>
+					<li><a href="employeeview.php">View Employees</a></li>
+				</ul>
+				</div>
+			</header>
+			<!-- 
+				possibly add a navigation for  contacting support
+			<nav>
+			
+				<p>
+					<a href="#">Our Home</a>
+				</p>
+				<p>
+					<a href="#">Contact support</a>
+				</p>
+			</nav>
+           -->
+			<div id="homepage">
+			    Internal Hospital Site <br>
+			    5391 Main Street <br>
+			    River Falls, WI 54022 <br>
+			    Call Extension 8201 for administration <br>
+			    Call Extension 3412 for front desk <br>
+			</div>
+
+			<footer>
+    			<p> &copy;  Copyright by River Falls Hospital</p>
+			</footer>
+		</div>
+	</body>
+</html>
